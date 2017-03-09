491d092cce796984e0bd6333e2e4feed123633ac
Lukas Woodtli <woodtli.lukas@gmail.com>
2017-03-09 21:12:03 +0100

    Improve cmake build

--- .travis_build.sh
+++ .travis_build.sh
@@ -1,13 +1,4 @@
-set -ue
-function build() {
-    pushd $1
-    rm -rf build
-    mkdir build
-    cd build
-    cmake -G 'Unix Makefiles' ..
-    make && make test
-    popd
-}
-
-build samek
-build weirich
+mkdir build
+cd build
+cmake -G 'Unix Makefiles' ..
+make && make test
